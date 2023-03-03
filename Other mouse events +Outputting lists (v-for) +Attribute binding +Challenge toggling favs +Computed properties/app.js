cc79cff2842517d99a59a127d74bd497c2ae13a2
shadowgks <saad.moumou.coder@gmail.com>
2023-03-03 15:47:04 +0100

    Outputting lists (v-for) DONE

--- Other mouse events +Outputting lists (v-for) +Attribute binding +Challenge toggling favs +Computed properties/app.js
+++ Other mouse events +Outputting lists (v-for) +Attribute binding +Challenge toggling favs +Computed properties/app.js
@@ -3,21 +3,39 @@ const app = Vue.createApp({
     // template:'<h1>Hello Vuejs</h1>',
     data() {
         return {
+            //B mouse event
             x: 0,
             y: 0,
+            //E mouse event
+            //B Outputting lists (v-for)
+            Books: [
+                {
+                    title: 'title1',
+                    author: 'auther1',
+                    pages: 30
+                },
+                {
+                    title: 'title2',
+                    author: 'auther2',
+                    pages: 100
+                },
+            ]
+            //E Outputting lists (v-for)
         }
     },
     methods: {
-        mouseEvent(e, data){
+        //B mouse event
+        mouseEvent(e, data) {
             console.log(e, e.type);
-            if(data){
+            if (data) {
                 console.log(data);
             }
         },
-        mouseMove(e){
+        mouseMove(e) {
             this.x = e.offsetX;
             this.y = e.offsetY;
         }
+        //E mouse event
     }
 });
 

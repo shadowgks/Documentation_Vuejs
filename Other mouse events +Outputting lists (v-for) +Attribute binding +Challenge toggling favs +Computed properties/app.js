a12da8d6644ef9d4b22f02e93e4f92a13afc274d
shadowgks <saad.moumou.coder@gmail.com>
2023-03-03 12:27:53 +0100

    mouse events done

--- Other mouse events +Outputting lists (v-for) +Attribute binding +Challenge toggling favs +Computed properties/app.js
+++ Other mouse events +Outputting lists (v-for) +Attribute binding +Challenge toggling favs +Computed properties/app.js
@@ -1,18 +1,25 @@
 // console.log('hello World');
 const app = Vue.createApp({
     // template:'<h1>Hello Vuejs</h1>',
-    data(){
+    data() {
         return {
-            x : 0,
-            y : 0,
+            x: 0,
+            y: 0,
         }
     },
-    methods:{
-        mouseEvent(){
-            console.log('work');
+    methods: {
+        mouseEvent(e, data){
+            console.log(e, e.type);
+            if(data){
+                console.log(data);
+            }
+        },
+        mouseMove(e){
+            this.x = e.offsetX;
+            this.y = e.offsetY;
         }
     }
 });
+
+//Parent
 app.mount('#app'); 
-
-

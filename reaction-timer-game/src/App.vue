e7d404ddce31f70d32a05999b7c026bb47cdd39a
shadowgks <saad.moumou.coder@gmail.com>
2023-03-06 14:44:29 +0100

    Lifecycle hooks methods

---
+++ reaction-timer-game/src/App.vue
@@ -0,0 +1,65 @@
+<template>
+  <h1>Reaction Timer</h1>
+  <button class="btn-start" :disabled='isplayin' v-on:click="start">Start</button>
+  <Block v-if="isplayin" v-bind:delay="delay"/>
+</template>
+
+<script>
+import Block from './components/Block.vue';
+export default {
+  components: { Block },
+  data() {
+    return {
+      delay: null,
+      isplayin: false,
+    };
+  },
+  methods: {
+    start() {
+      this.delay = 2000 + Math.random() * 5000;
+      this.isplayin = true;
+      console.log(this.delay);
+    },
+  },
+};
+</script>
+
+<style scoped>
+header {
+  line-height: 1.5;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+.btn-start{
+  width: 100%;
+  padding: 10px;
+  background-color: brown;
+  color: #fff;
+  border: none;
+  cursor: pointer;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+}
+button:disabled{
+    cursor: not-allowed;
+}
+</style>
---
+++ reaction-timer-game/src/components/Block.vue
@@ -0,0 +1,39 @@
+<template>
+    <div class="block" v-if="showBlock" @click="stopTimer"> 
+        <p>Click Here</p>
+    </div>
+</template>
+
+<script>
+export default {
+    props: ['delay'],
+    data() {
+        return {
+            showBlock : false,
+        }
+    },
+    mounted() {
+        setTimeout(() => {
+            this.showBlock = !this.showBlock;
+        }, this.delay);
+    },
+    updated() {
+        
+    },
+    unmounted(){
+        
+    }
+};
+</script>
+
+<style>
+.block{
+    width: 100%;
+    background-color: #fff;
+    color: #000;
+    text-align: center;
+    padding: 60px;
+    margin: 20px auto;
+    cursor: pointer;
+}
+</style>
